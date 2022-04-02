a9a3d023d082f6c819cba7d20b89907b04ba3208
Jonathan Cameron <jonathancameron6@protonmail.com>
2022-04-01 21:26:21 -0700

    Finish button styles

--- src/button/button.js
+++ src/button/button.js
@@ -3,5 +3,40 @@ import './button.scss'
 
 export default function Button(props) {
 
-  return (<div className={'the-div'}></div>)
+  return (
+    <div
+     className={`
+       structure-lib-button-wrap
+      ${props.fullWidth ? `full-width` : ``}
+      ${props.loading ? `loading` : ``}
+      ${props.disabled ? `disabled` : ``}
+      `}
+     onClick={() => {
+         if(props.onClick){
+           props.onClick();
+         }
+       }
+     }
+    >
+      <div
+        className={`button-flex`}
+      >
+        {props.icon &&
+          <div
+            className={`button-icon fa ${props.icon}`}
+          />
+        }
+        {props.loading &&
+          <div
+          className={`loading-spinner fa fa-spinner`}
+          />
+        }
+        <div
+          className={`button-text`}
+        >
+          {props.text || `Click Me!`}
+        </div>
+      </div>
+    </div>
+);
 }
